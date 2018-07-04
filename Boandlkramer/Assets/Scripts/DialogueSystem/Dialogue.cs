0e1887aa89132eed2426655d6976bb9b23f3a8d4
Felix <felix.kammerlander@gmail.com>
2018-07-05 00:02:38 +0200

    dialogue system v0.1

---
+++ Boandlkramer/Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+[System.Serializable]
+public struct Reply
+{
+	public string reply;
+	public Dialogue replyDialogue;
+}
+
+[System.Serializable]
+public class Dialogue {
+
+	public string name;
+	public string text;
+	// public string[] requiredTags;
+	public List<Reply> replies;
+}
---
+++ Boandlkramer/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+	// sliding UI in and out
+	public Animator animator;
+
+	// add dialogue text and replys to this game object
+	public GameObject contentParent;
+
+	// Texts and Replys to create
+	[SerializeField]
+	GameObject dialogueTextBoxPrefab;
+	[SerializeField]
+	GameObject replyPrefab;
+
+	List<GameObject> currentReplies = new List<GameObject>();
+
+	public void StartDialogue(Dialogue DialogueNode)
+	{
+		animator.SetBool("isOpen", true);
+		AddDialogue(DialogueNode);
+	}
+
+
+	public void EndDialogue()
+	{
+		// delete everything such that we can start from a plain dialogue ui next time...
+		int iElements = contentParent.transform.childCount;
+		for (int i = 0; i < iElements; i++)
+		{
+			var child = contentParent.transform.GetChild(i);
+			Destroy(child.gameObject);
+
+		}
+		animator.SetBool("isOpen", false);
+		Debug.Log("close dialogue animation");
+	}
+
+	void AddDialogue(Dialogue dialogue)
+	{
+		/*
+		// check if there are valid replies, if so, then delete them
+		if (currentReplies.Count > 0)
+		{
+			// look for the reply given
+			for (int i = 0; i < currentReplies.Count; i++)
+			{
+				if (currentReplies[i].GetComponent<Dia>)
+			}
+		}
+		*/
+
+		// create a text box containing the sentence
+		string text = "<color=yellow>" + dialogue.name + ":</color> " + dialogue.text;
+
+		GameObject go = Instantiate(dialogueTextBoxPrefab, contentParent.transform);
+		go.GetComponent<Text>().text = text + "\n";
+
+		// next, add possible replies
+		for (int i = 0; i < dialogue.replies.Count; i++)
+		{
+			GameObject replyGO = Instantiate(replyPrefab, contentParent.transform);
+			replyGO.GetComponent<Text>().text = (i+1) + ") " + dialogue.replies[i].reply;
+
+			if (dialogue.replies[i].replyDialogue.text == "")
+			{
+				// if there is no next dialogue to this reply, we add and "[Ende]" tag in order to indicate that this answer will 
+				// end the dialogue.
+				replyGO.GetComponent<Text>().text += " [Ende]";
+			}
+
+			replyGO.GetComponent<DialogueReplyButton>().key = i + 1;								// set hotkey to associated keyboard key
+			replyGO.GetComponent<DialogueReplyButton>().next = dialogue.replies[i].replyDialogue;	// link next dialogue
+
+			// for the last element we add a new line
+			if (i == dialogue.replies.Count - 1)
+			{
+				replyGO.GetComponent<Text>().text += "\n";
+
+			}
+			currentReplies.Add(replyGO);
+		}
+	}
+}
