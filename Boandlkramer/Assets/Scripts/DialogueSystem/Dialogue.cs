d1babdd382554ce40efb30f70e7fda10773ec13e
Felix <felix.kammerlander@gmail.com>
2018-07-19 21:58:17 +0200

    bug fixing dialogue ui animation

--- Boandlkramer/Assets/Scripts/DialogueSystem/Dialogue.cs
+++ Boandlkramer/Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -6,15 +6,24 @@ using UnityEngine;
 [System.Serializable]
 public struct Reply
 {
+    // text of this reply
 	public string reply;
+
+    // link to next dialogue block called when this reply is given by the player
 	public Dialogue replyDialogue;
 }
 
 [System.Serializable]
 public class Dialogue {
 
+    // name of the character saying these lines
 	public string name;
+
+    // content of this dialogue block
 	public string text;
+
 	// public string[] requiredTags;
+
+    // possible replies to this dialogue block
 	public List<Reply> replies;
 }
